054ab182d5ef628605d28c36e5c67938e9994976
Carrie Furet <carrie.furet@gmail.com>
2026-03-20 19:38:39 -0700

    Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8cEJTsTlobq4bFjVfLxoHf1epxTK02ps",
-  authDomain: "glove-story.firebaseapp.com",
-  projectId: "glove-story",
-  storageBucket: "glove-story.firebasestorage.app",
-  messagingSenderId: "309260866475",
-  appId: "1:309260866475:web:4a6cf2c757465b8e0ab8ee"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
